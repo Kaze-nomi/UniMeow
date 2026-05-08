e8a485d1342d3d88a5772b374875fb7e6224b115
Kazenomi <Artem080605@gmail.com>
2026-05-08 15:59:11 +0300

    Repository fix

--- UserService/src/main/java/uni/user/repository/FacultyProposalRepository.java
+++ UserService/src/main/java/uni/user/repository/FacultyProposalRepository.java
@@ -1,7 +1,13 @@
 package uni.user.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uni.user.entity.FacultyProposal;
 
 public interface FacultyProposalRepository extends JpaRepository<FacultyProposal, Long> {
+	@Override
+	@EntityGraph(attributePaths = "university")
+	List<FacultyProposal> findAll();
 }
--- UserService/src/main/java/uni/user/repository/ProgramProposalRepository.java
+++ UserService/src/main/java/uni/user/repository/ProgramProposalRepository.java
@@ -1,7 +1,13 @@
 package uni.user.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uni.user.entity.ProgramProposal;
 
 public interface ProgramProposalRepository extends JpaRepository<ProgramProposal, Long> {
+	@Override
+	@EntityGraph(attributePaths = { "university", "faculty" })
+	List<ProgramProposal> findAll();
 }
